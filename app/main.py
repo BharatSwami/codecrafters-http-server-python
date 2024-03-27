f6d63b006db222519d3418037cc5c514ddf82de6
Bharat Swami <bharatswami1299@gmail.com>
2024-03-27 12:56:12 +0530

    user-agent

--- app/main.py
+++ app/main.py
@@ -18,19 +18,24 @@ def main():
             #print(data)
             if not data:
                 break
-            data_list = data.split("\n")[0].split(" ")
-            print(data_list)
-            if "echo/" in data_list[1]:
-                random_string = data_list[1].split("echo")[-1]
-                print(random_string)
-                response_massage = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(random_string)-1}\r\n\r\n{random_string[1:]}"
-                print(response_massage)
-                conn.sendall(response_massage.encode())
-            elif data_list[1] == "/":
-                conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-            else:
-                conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
-    
+            startline, host, UserAgent = data.split("\n")
+            request_verb, request_target, http_version = startline.split(" ")
+            if request_verb == "GET":
+                response_massage = ""
+                if "echo/" in request_target:
+                    random_string = request_target.split("echo/")[-1]
+                    #print(request_target)
+                    response_massage = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(request_target)}\r\n\r\n{request_target}"
+                    #print(response_massage)
+                elif request_target = "/user-agent":
+                    user_agent = UserAgent.split(" ")[-1]
+                    response_massage = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
+                elif request_target == "/":
+                    response_massage = "HTTP/1.1 200 OK\r\n\r\n"
+                else:
+                    response_massage = "HTTP/1.1 404 Not Found\r\n\r\n"
+            
+            conn.sendall(response_massage.encode())
     
     server_socket.close()
 

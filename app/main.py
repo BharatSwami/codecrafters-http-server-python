b44f5586cf1a2c8c330a28e6a1a2aa868cc1d1d4
Bharat Swami <bharatswami1299@gmail.com>
2024-03-27 12:27:08 +0530

    body p3

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ def main():
             data_list = data.split("\n")[0].split(" ")
             print(data_list)
             if data_list[1].startswith("/echo/"):
-                random_string = data_list[1]/split("/")[-1]
+                random_string = data_list[1].split("echo")[-1]
                 print(random_string)
                 response_massage = f"HTTP/1.1 200 OK\r\n\r\nContent-Type: text/plain\n\r\n\rContent-Length: 3\n\r\n\r\n{random_string}\n\r\n\r"
                 conn.sendall(response_massage.encode())

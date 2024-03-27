fb86d97d9dc0dfa5e86d766fbe80f5505dd58bbe
Bharat Swami <bharatswami1299@gmail.com>
2024-03-27 13:03:22 +0530

    user-agent p3

--- app/main.py
+++ app/main.py
@@ -20,20 +20,22 @@ def main():
                 break
             startline, host, UserAgent = data.split("\n")
             request_verb, request_target, http_version = startline.split(" ")
-            if request_verb == "GET":
-                response_massage = ""
-                if "echo/" in request_target:
-                    random_string = request_target.split("echo/")[-1]
-                    #print(request_target)
-                    response_massage = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(random_string)}\r\n\r\n{random_string}"
-                    #print(response_massage)
-                elif request_target == "/user-agent":
-                    user_agent = UserAgent.split(" ")[-1]
-                    response_massage = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
-                elif request_target == "/":
-                    response_massage = "HTTP/1.1 200 OK\r\n\r\n"
-                else:
-                    response_massage = "HTTP/1.1 404 Not Found\r\n\r\n"
+            #if request_verb == "GET":
+            response_massage = ""
+            print(startline, host, UserAgent)
+            print(request_verb, request_target, http_version)
+            if "echo/" in request_target:
+                random_string = request_target.split("echo/")[-1]
+                #print(request_target)
+                response_massage = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(random_string)}\r\n\r\n{random_string}"
+                #print(response_massage)
+            elif request_target == "/user-agent":
+                user_agent = UserAgent.split(" ")[-1]
+                response_massage = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
+            elif request_target == "/":
+                response_massage = "HTTP/1.1 200 OK\r\n\r\n"
+            else:
+                response_massage = "HTTP/1.1 404 Not Found\r\n\r\n"
             #
             conn.sendall(response_massage.encode())
     

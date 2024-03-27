28b3342f4ac9797dd42925db9d85cf2c02b26a16
Bharat Swami <bharatswami1299@gmail.com>
2024-03-27 13:14:21 +0530

    user-agentp4 3

--- app/main.py
+++ app/main.py
@@ -27,17 +27,17 @@ def main():
             response_massage = ""
             print(startline_list)
             #print(request_verb, request_target, http_version)
-            if "echo/" in startline_list[1]:#request_target:
+            if "echo/" in startline_list[1]:    #request_target:
                 #random_string = request_target.split("echo/")[-1]
                 random_string = startline_list[1].split("echo/")[-1]
                 #print(request_target)
                 response_massage = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(random_string)}\r\n\r\n{random_string}"
                 #print(response_massage)
-            elif startline_list[1] == "/user-agent"#request_target == "/user-agent":
+            elif startline_list[1] == "/user-agent":   #request_target == "/user-agent":
                 #user_agent = UserAgent.split(" ")[-1]
                 user_agent = data_list[2].split(" ")[-1]
                 response_massage = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
-            elif startline_list[1] == "/"#request_target == "/":
+            elif startline_list[1] == "/":   #request_target == "/":
                 response_massage = "HTTP/1.1 200 OK\r\n\r\n"
             else:
                 response_massage = "HTTP/1.1 404 Not Found\r\n\r\n"

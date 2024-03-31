f1b2be8d2720abaa24e16e3946f245c07aef160d
Bharat Swami <bharatswami1299@gmail.com>
2024-03-31 10:52:34 +0530

    hello

--- app/main.py
+++ app/main.py
@@ -23,47 +23,65 @@ def handle_response(conn,addr,directory = " "):
             #print(data_list)
             #request_verb, request_target, http_version = startline.split(" ")
             startline_list = data_list[0].split(" ")
-            #if request_verb == "GET":
-            response_massage = ""
-            #print(startline_list)
-            #print(request_verb, request_target, http_version)
-            if "echo/" in startline_list[1]:    #request_target:
-                #random_string = request_target.split("echo/")[-1]
-                random_string = startline_list[1].split("echo/")[-1]
-                #print(request_target)
-                response_massage = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(random_string)}\r\n\r\n{random_string}"
-                #print(response_massage)
-            elif "files/" in startline_list[1]:
-                filename = startline_list[1].split("/")[-1]
-                print(filename)
-                absolute_filepath = os.path.join(directory,filename)
-                print(absolute_filepath)
-                
-                if os.path.exists(absolute_filepath) and os.path.isfile(absolute_filepath):
-                        with open(absolute_filepath, 'rb') as f:
-                            contents = f.read().decode()
-                            #print(type(contents))
-
-                            response_massage = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(contents)}\r\n\r\n" + contents
+            if startline_list[0] == "GET":
+                response_massage = ""
+                #print(startline_list)
+                #print(request_verb, request_target, http_version)
+                if "echo/" in startline_list[1]:    #request_target:
+                    #random_string = request_target.split("echo/")[-1]
+                    random_string = startline_list[1].split("echo/")[-1]
+                    #print(request_target)
+                    response_massage = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(random_string)}\r\n\r\n{random_string}"
+                    #print(response_massage)
+                elif "files/" in startline_list[1]:
+                    filename = startline_list[1].split("/")[-1]
+                    print(filename)
+                    absolute_filepath = os.path.join(directory,filename)
+                    print(absolute_filepath)
                     
-                else:  
-                    response_massage = "HTTP/1.1 404 Not Found\r\n\r\n"
-                    #
-                    ###
-                
+                    if os.path.exists(absolute_filepath) and os.path.isfile(absolute_filepath):
+                            with open(absolute_filepath, 'rb') as f:
+                                contents = f.read().decode()
+                                #print(type(contents))
 
-                print(response_massage)
-            elif startline_list[1] == "/user-agent":   #request_target == "/user-agent":
-                #user_agent = UserAgent.split(" ")[-1]
-                user_agent = data_list[2].split("\r")[0].split(" ")[-1]
-                print(user_agent)
-                response_massage = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
-            elif startline_list[1] == "/":   #request_target == "/":
-                response_massage = "HTTP/1.1 200 OK\r\n\r\n"
-            else:
-                response_massage = "HTTP/1.1 404 Not Found\r\n\r\n"
-            #
-            conn.send(response_massage.encode())
+                                response_massage = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(contents)}\r\n\r\n" + contents
+                                f.close()
+                    else:  
+                        response_massage = "HTTP/1.1 404 Not Found\r\n\r\n"
+                        #
+                        ###
+                    
+
+                    print(response_massage)
+                elif startline_list[1] == "/user-agent":   #request_target == "/user-agent":
+                    #user_agent = UserAgent.split(" ")[-1]
+                    user_agent = data_list[2].split("\r")[0].split(" ")[-1]
+                    print(user_agent)
+                    response_massage = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
+                elif startline_list[1] == "/":   #request_target == "/":
+                    response_massage = "HTTP/1.1 200 OK\r\n\r\n"
+                else:
+                    response_massage = "HTTP/1.1 404 Not Found\r\n\r\n"
+                #
+                conn.send(response_massage.encode())
+            elif startline_list[0] == "POST":
+                if "files/" in startline_list[1]:
+                    filename = startline_list[1].split("/")[-1]
+                    print(filename)
+                    absolute_filepath = os.path.join(directory,filename)
+                    print(absolute_filepath)
+                    
+                    if os.path.exists(absolute_filepath) and os.path.isfile(absolute_filepath):
+                            with open(absolute_filepath, 'w') as f:
+                                print(data_list[-1])
+                                f.write(data_list[-1])
+                                #print(type(contents))
+
+                                response_massage = f"HTTP/1.1 201 Created\r\n\r\n"
+                                f.close()
+                    else:  
+                        response_massage = "HTTP/1.1 404 Not Found\r\n\r\n"
+                conn.send(response_massage.encode())
             conn.close()
         
 def main():
